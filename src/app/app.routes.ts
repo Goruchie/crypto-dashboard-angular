d2736ffeb59a46b21a47fc6d2fd0559a63a66f59
Goruchie <marcosgualtero43253@gmail.com>
2025-05-05 15:19:33 -0300

    Create the Header Component and implement it with the routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { PriceHistoryComponent } from './pages/price-history/price-history.component';
+import { ComparisonComponent } from './pages/comparison/comparison.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    
+    { path: '', component: DashboardComponent },
+    { path: 'price-history', component: PriceHistoryComponent },
+    { path: 'comparison', component: ComparisonComponent },
+
+];
